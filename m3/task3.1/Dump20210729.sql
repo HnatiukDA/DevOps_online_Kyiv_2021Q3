b872f7b9e3db9fae5ef409e263c7009ef0c06d91
Dmytro Hnatiuk <gnatyuk.d.a.95@gmail.com>
2021-07-29 21:10:50 +0300

    Added few images and Dump from AWS RDS MySQL DB

---
+++ m3/task3.1/Dump20210729.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: db-1.cxaklrgha65a.eu-central-1.rds.amazonaws.com    Database: MYSQL_from_backup
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `docs`
+--
+
+DROP TABLE IF EXISTS `docs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `docs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `people_id` int DEFAULT NULL,
+  `doc_name` varchar(100) DEFAULT NULL,
+  `doc_num` varchar(100) DEFAULT NULL,
+  `doc_authority` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `docs`
+--
+
+LOCK TABLES `docs` WRITE;
+/*!40000 ALTER TABLE `docs` DISABLE KEYS */;
+INSERT INTO `docs` VALUES (1,1,'pass','004638219','8511'),(2,2,'pass','005123219','8511'),(3,2,'foreighn pass','FS554876','8511'),(4,3,'pass','004638219','8511'),(5,4,'pass','004638219','8511');
+/*!40000 ALTER TABLE `docs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hobby`
+--
+
+DROP TABLE IF EXISTS `hobby`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hobby` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `people_id` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hobby`
+--
+
+LOCK TABLES `hobby` WRITE;
+/*!40000 ALTER TABLE `hobby` DISABLE KEYS */;
+INSERT INTO `hobby` VALUES (1,1,'Reading'),(2,1,'Movies'),(3,2,'Swimming'),(4,3,'Skateboarding'),(5,4,'Workout'),(6,1,'Horse riding'),(7,1,'Reading'),(8,1,'Movies'),(9,2,'Swimming'),(10,3,'Skateboarding'),(11,4,'Workout'),(12,1,'Horse riding');
+/*!40000 ALTER TABLE `hobby` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `people` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `sex` varchar(20) DEFAULT NULL,
+  `birth` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+INSERT INTO `people` VALUES (1,'Dmytro','Kyiv','male','1995-01-10'),(2,'Olga','Kharkiv','female','1993-12-27'),(3,'Max','Lviv','male','1994-08-29'),(4,'Viacheslav','Kharkiv','male','1995-02-14'),(5,'Sergey','Odessa','male','1984-04-21');
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-29 21:09:44
